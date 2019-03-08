0e901c4665d389352af1d1cb60df3c8d6206d6e3
Thomas Claireau <thomas.claireau@gmail.com>
2019-03-08 09:45:38 +0100

    fix slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('.menu > li a').on('click', function() {
+    $('.menu > li a').on('click', function () {
         $('.menu > li a.active').removeClass('active');
         $(this).addClass('active');
     });
 
-    $('.icon-menu').on('click', function() {
+    $('.icon-menu').on('click', function () {
         if ($('.menu').attr('class') == 'menu menu-disabled') {
             $('.menu').removeClass('menu-disabled');
             $('.menu').addClass('menu-active');
@@ -17,10 +17,11 @@ $(document).ready(function() {
 
     //SLIDER
     let $carousel = $('.slider'),
-    $img = $('.slider .diapo'),
-    indexImg = $img.length - 1,
-    i = 0,
-    $currentImg = $img.eq(i);
+        $img = $('.slider .diapo'),
+        indexImg = $img.length - 1,
+        i = 0,
+        clicked = 1,
+        $currentImg = $img.eq(i);
 
     $img.css('display', 'none');
     $currentImg.css('display', 'block');
@@ -28,7 +29,7 @@ $(document).ready(function() {
     // ARROW
     $carousel.append('<div class="arrow arrow-prev"></div><div class="arrow arrow-next"></div>');
 
-    $('.arrow-next').on("click", function() {
+    $('.arrow-next').on("click", function () {
         i++;
 
         if (i <= indexImg) {
@@ -41,9 +42,21 @@ $(document).ready(function() {
             $currentImg = $img.eq(i);
             $currentImg.css('display', 'block');
         }
+
+        clicked = clicked + 1;
+
+        if (clicked <= 3) {
+            $('.carousel-buttons').removeClass("active");
+            $('#carousel' + clicked).addClass("active");
+        } else {
+            clicked = 1;
+            $('.carousel-buttons').removeClass("active");
+            $('#carousel' + clicked).addClass("active");
+        }
+
     });
 
-    $('.arrow-prev').on("click", function() {
+    $('.arrow-prev').on("click", function () {
         i--;
 
         if (i >= 0) {
@@ -56,11 +69,22 @@ $(document).ready(function() {
             $currentImg = $img.eq(i);
             $currentImg.css('display', 'block');
         }
+
+        clicked = clicked - 1;
+
+        if (clicked > 0) {
+            $('.carousel-buttons').removeClass("active");
+            $('#carousel' + clicked).addClass("active");
+        } else {
+            clicked = indexImg + 1;
+            $('.carousel-buttons').removeClass("active");
+            $('#carousel' + clicked).addClass("active");
+        }
     });
 
     // Defilement auto
     function slideImg() {
-        setTimeout(function() {
+        setTimeout(function () {
             if (i < indexImg) {
                 i++;
             } else {
@@ -71,6 +95,10 @@ $(document).ready(function() {
             $currentImg = $img.eq(i);
             $currentImg.css('display', 'block');
 
+            clicked = i + 1;
+            $('.carousel-buttons').removeClass("active");
+            $('#carousel' + clicked).addClass("active");
+
             slideImg();
         }, 4000);
     }
@@ -78,46 +106,53 @@ $(document).ready(function() {
     slideImg();
 
     // DOTS
-/*     $carousel.append('<ul class="dots">');
+    $carousel.append('<div class="carousel-buttons-container"><ul class="dots">');
+    let addId = i + 1;
 
     for (let a = 0; a <= indexImg; a++) {
-        $('.dots').append('<li class="dot" data-index="' + i + '">');
+        $('.dots').append('<li class="carousel-buttons" id="carousel' + addId++ + '"></li>');
     }
 
-    $nextSlide = indexImg + 1;
+    $('.carousel-buttons').click(function () {
+        let findIdClicked = $(this).attr("id");
+        let splitString = findIdClicked.split("carousel");
+        let findTheNumb = splitString[1];
 
-    $carousel.find(".dots").find(".dot").addClass('active'); */
+        $img.css('display', 'none');
+        $currentImg = $img.eq(findTheNumb - 1);
+        $currentImg.css('display', 'block');
+
+        $(".carousel-buttons").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $('.carousel-buttons-container').find("li").first().addClass("active");
+
+
 
 
     // IMAGES ANIMEES
-    $('#col-1').mouseover(function() {
+    $('#col-1').mouseover(function () {
         $('#col-1 img').attr('src', './img/lego-enjoy-plus.png');
     });
 
-    $('#col-1').mouseout(function() {
+    $('#col-1').mouseout(function () {
         $('#col-1 img').attr('src', './img/lego-enjoy.png');
     });
 
-    $('#col-2').mouseover(function() {
+    $('#col-2').mouseover(function () {
         $('#col-2 img').attr('src', './img/briques-lego-assemblees.png');
     });
 
-    $('#col-2').mouseout(function() {
+    $('#col-2').mouseout(function () {
         $('#col-2 img').attr('src', './img/briques-lego.png');
     });
 
-    $('#col-3').mouseover(function() {
+    $('#col-3').mouseover(function () {
         $('#col-3 img').attr('src', './img/travailleur-lego-hands-up.png');
     });
 
-    $('#col-3').mouseout(function() {
+    $('#col-3').mouseout(function () {
         $('#col-3 img').attr('src', './img/travailleur-lego.png');
     });
-
-
 });
-
-
-
-
-
