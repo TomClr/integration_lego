9f926652974060cbc2241bce5ffa8a7e03e84196
Thomas Claireau <thomas.claireau@gmail.com>
2019-03-09 14:28:07 +0100

    fix slider and small errors

--- js/scripts.js
+++ js/scripts.js
@@ -9,132 +9,15 @@ $(document).ready(function () {
         if ($('.menu').attr('class') == 'menu menu-disabled') {
             $('.menu').removeClass('menu-disabled');
             $('.menu').addClass('menu-active');
+            
+            /* if ($(window).width() < 715) {
+                $('section.slider .arrow').css('top', '125%');
+            } */
         } else if ($('.menu').attr('class') == 'menu menu-active') {
             $('.menu').removeClass('menu-active');
             $('.menu').addClass('menu-disabled');
         };
     });
-
-/*     //SLIDER
-    let $carousel = $('.slider'),
-        $img = $('.slider .diapo'),
-        indexImg = $img.length - 1,
-        i = 0,
-        clicked = 1,
-        hasBeenClicked = false;
-        $currentImg = $img.eq(i);
-
-    $img.css('display', 'none');
-    $currentImg.css('display', 'block');
-
-    // BARRE DEFILEMENT
-
-
-    // ARROW
-    $carousel.append('<div class="arrow arrow-prev"></div><div class="arrow arrow-next"></div>');
-
-    $('.arrow-next').on("click", function () {
-        hasBeenClicked = true;
-        i++;
-
-        if (i <= indexImg) {
-            $img.css('display', 'none');
-            $currentImg = $img.eq(i);
-            $currentImg.fadeIn("slow");
-        } else {
-            i = 0;
-            $img.css('display', 'none');
-            $currentImg = $img.eq(i);
-            $currentImg.fadeIn("slow");
-        }
-
-        clicked = clicked + 1;
-
-        if (clicked <= 3) {
-            $('.carousel-buttons').removeClass("active");
-            $('#carousel' + clicked).addClass("active");
-        } else {
-            clicked = 1;
-            $('.carousel-buttons').removeClass("active");
-            $('#carousel' + clicked).addClass("active");
-        }
-
-    });
-
-    $('.arrow-prev').on("click", function () {
-        i--;
-
-        if (i >= 0) {
-            $img.css('display', 'none');
-            $currentImg = $img.eq(i);
-            $currentImg.fadeIn("slow");
-        } else {
-            i = indexImg;
-            $img.css('display', 'none');
-            $currentImg = $img.eq(i);
-            $currentImg.fadeIn("slow");
-        }
-
-        clicked = clicked - 1;
-
-        if (clicked > 0) {
-            $('.carousel-buttons').removeClass("active");
-            $('#carousel' + clicked).addClass("active");
-        } else {
-            clicked = indexImg + 1;
-            $('.carousel-buttons').removeClass("active");
-            $('#carousel' + clicked).addClass("active");
-        }
-    });
-
-    // Defilement auto
-    function slideImg() {
-        let timeOutHandle = setInterval(function () {
-            if (i < indexImg) {
-                i++;
-            } else {
-                i = 0;
-            }
-
-            $img.css('display', 'none');
-            $currentImg = $img.eq(i);
-            $currentImg.fadeIn("slow");
-
-            clicked = i + 1;
-            $('.carousel-buttons').removeClass("active");
-            $('#carousel' + clicked).addClass("active");
-            
-        }, 4000);
-        
-    }
-
-    slideImg();
-
-    // DOTS
-    $carousel.append('<div class="carousel-buttons-container"><ul class="dots">');
-    let addId = i + 1;
-
-    for (let a = 0; a <= indexImg; a++) {
-        $('.dots').append('<li class="carousel-buttons" id="carousel' + addId++ + '"></li>');
-    }
-
-    $('.carousel-buttons').click(function () {
-        let findIdClicked = $(this).attr("id");
-        let splitString = findIdClicked.split("carousel");
-        let findTheNumb = splitString[1];
-
-        $img.css('display', 'none');
-        $currentImg = $img.eq(findTheNumb - 1);
-        $currentImg.fadeIn("slow");
-
-        $(".carousel-buttons").removeClass("active");
-        $(this).addClass("active");
-    });
-
-    $('.carousel-buttons-container').find("li").first().addClass("active"); */
-
-
-
 
     // IMAGES ANIMEES
     $('#col-1').mouseover(function () {
--- js/slider.js
+++ js/slider.js
@@ -1,232 +1,182 @@
-jQuery(document).ready(function () {
+//SLIDER
+startSlider($('#slider'), 20);
 
-    startSlider($('#slider'), 20); // Slide container ID, SlideShow interval 
+function startSlider(obj, timer) {
 
-    function startSlider(obj, timer) {
+    let id = "#" + obj.attr("id");
+    let slideCount = obj.find('.diapo').length;
+    slideWidth = obj.attr("data-width");
+    let sliderUlWidth = (slideCount + 1) * slideWidth;
+    let time = 2;
+    let $bar,
 
-        let id = "#" + obj.attr("id");
-        let slideCount = obj.find('.slideUl li').length;
-        slideWidth = obj.attr("data-width");
-        let sliderUlWidth = (slideCount + 1) * slideWidth;
-        let time = 2;
-        let $bar,
+        isPause,
+        tick,
+        percentTime;
+    isPause = false;
 
-            isPause,
-            tick,
-            percentTime;
-        isPause = false; //false for auto slideshow
+    let $carousel = $('.slider'),
+        $img = $('.slider .diapo'),
+        indexImg = $img.length - 1,
+        i = 0,
+        clicked = 1,
+        hasBeenClicked = false;
+    $currentImg = $img.eq(i);
 
-        $bar = obj.find('.progress .bar');
+    $img.css('display', 'none');
+    $currentImg.css('display', 'block');
 
-        // ARROW
-        /* obj.append('<div class="control control_next"></div><div class="control control_prev"></div>'); */
+    $bar = obj.find('.progress .bar');
 
-        // DOTS
-        obj.append('<div class="carousel-buttons-container"><ul class="dots">');
-        let i = 0,
-            addId = i + 1,
-            $img = $('.slideUl .slide'),
-            indexImg = $img.lenght - 1,
-            clicked = 1,
-            $currentImg = $img.eq(i);
-
-        for (let a = 0; a <= slideCount - 1; a++) {
-            $('.dots').append('<li class="carousel-buttons" id="carousel' + addId++ + '"></li>');
-        };
-
-        $('#carousel1').addClass('active');
-
-        /* $('.control_next').on("click", function () {
-            clicked = clicked + 1;
-
-            if (clicked <= 3) {
-                $('.carousel-buttons').removeClass("active");
-                $('#carousel' + clicked).addClass("active");
-            } else {
-                clicked = 1;
-                $('.carousel-buttons').removeClass("active");
-                $('#carousel' + clicked).addClass("active");
-            }
-        }); */
-
-        /* $('.control_prev').on("click", function () {
-            clicked = clicked - 1;
-
-            if (clicked > 0) {
-                $('.carousel-buttons').removeClass("active");
-                $('#carousel' + clicked).addClass("active");
-            } else {
-                clicked = indexImg + 1;
-                $('.carousel-buttons').removeClass("active");
-                $('#carousel' + clicked).addClass("active");
-            }
-        }); */
+    function startProgressbar() {
+        resetProgressbar();
+        percentTime = 0;
+        tick = setInterval(interval, timer);
+    }
 
 
 
-
-        function startProgressbar() {
-            resetProgressbar();
-            percentTime = 0;
-            tick = setInterval(interval, timer);
-        }
-
-        function interval() {
-            if (isPause === false) {
-                percentTime += 1 / (time + 0.1);
-                $bar.css({
-                    width: percentTime + "%"
-                });
-                if (percentTime >= 100) {
-                    moveRight();
-                    startProgressbar();
-                }
-            }
-        }
-
-        function resetProgressbar() {
+    function interval() {
+        if (isPause === false) {
+            percentTime += 1 / (time + 0.1);
             $bar.css({
-                width: 0 + '%'
+                width: percentTime + "%"
             });
-            clearTimeout(tick);
+            if (percentTime >= 100) {
+                startProgressbar();
+                slideImg();
+            } 
         }
+    }
 
-        function startslide() {
+    function resetProgressbar() {
+        $bar.css({
+            width: 0 + '%'
+        });
+        clearTimeout(tick);
+    }
 
-            $(id + ' .slideUl li:last-child').prependTo(id + ' .slideUl');
-            obj.find('.slideUl').css({
-                width: sliderUlWidth + 'vw',
-                marginLeft: -slideWidth + 'vw'
-            });
+    function startslide() {
 
-            obj.find('.slideUl li:last-child').appendTo(obj.attr('id') + ' .slideUl');
-
-        }
-
-        if (slideCount > 1) {
-            startslide();
-            startProgressbar();
-        } else { // hade navigation buttons for 1 slide only
-            $(id + ' .control_prev').hide();
-            $(id + ' .control_next').hide();
-        }
-
-
-
-
-        function moveLeft() {
-            $(id + ' .slideUl').css({
-                transition: "1s",
-                transform: "translateX(" + slideWidth + "vw)"
-            });
-
-            setTimeout(function () {
-
-                $(id + ' .slideUl li:last-child').prependTo(id + ' .slideUl');
-                $(id + ' .slideUl').css({
-                    transition: "none",
-                    transform: "translateX(" + 0 + "vw)"
-                });
-
-                $('li.actslide').prev().addClass('actslide').next().removeClass('actslide');
-            }, 1000);
-
-            clicked = clicked - 1;
-
-            if (clicked > 0) {
-                $('.carousel-buttons').removeClass("active");
-                $('#carousel' + clicked).addClass("active");
-            } else {
-                clicked = slideCount;
-                $('.carousel-buttons').removeClass("active");
-                $('#carousel' + clicked).addClass("active");
-            }
-
-        }
-
-        function moveRight2() { // fix for only 2 slades
-            $(id + ' .slideUl li:first-child').appendTo(id + ' .slideUl');
-
-
-            $(id + ' .slideUl').css({
-                transition: "none",
-                transform: "translateX(100vw)"
-            }).delay();
-
-            setTimeout(function () {
-
-                $(id + ' .slideUl').css({
-                    transition: "1s",
-                    transform: "translateX(0vw)"
-                });
-
-
-            }, 100, setTimeout(function () {
-
-
-                $(id + ' .slideUl').css({
-                    transition: "none",
-                    transform: "translateX(0vw)"
-                });
-                $('li.actslide').next().addClass('actslide').prev().removeClass('actslide');
-
-            }, 1000));
-
-
-
-
-        }
-
-        function moveRight() {
-            if (slideCount > 2) {
-                $(id + ' .slideUl').css({
-                    transition: "1s",
-                    transform: "translateX(" + (-1) * slideWidth + "vw)"
-                });
-
-                setTimeout(function () {
-
-                    $(id + ' .slideUl li:first-child').appendTo(id + ' .slideUl');
-                    $(id + ' .slideUl').css({
-                        transition: "none",
-                        transform: "translateX(" + 0 + "vw)"
-                    });
-
-                    $('li.actslide').next().addClass('actslide').prev().removeClass('actslide');
-                }, 1000);
-                clicked = clicked + 1;
-
-                if (clicked <= 3) {
-                    $('.carousel-buttons').removeClass("active");
-                    $('#carousel' + clicked).addClass("active");
-                } else {
-                    clicked = 1;
-                    $('.carousel-buttons').removeClass("active");
-                    $('#carousel' + clicked).addClass("active");
-                }
-            } else {
-                moveRight2();
-            }
-        }
-
-        $(id + ' .control_prev').click(function () {
-            moveLeft();
-            startProgressbar();
+        $(id + ' .slideUl li:first-child').prependTo(id + ' .slideUl');
+        obj.find('.slideUl').css({
+            width: sliderUlWidth + 'vw',
+            marginLeft: -slideWidth + 'vw'
         });
 
-        $(id + ' .control_next').click(function () {
+        obj.find('.slideUl li:first-child').appendTo(obj.attr('id') + ' .slideUl');
 
-            moveRight();
+    }
 
-            startProgressbar();
-        });
+    if (slideCount > 1) {
+        startslide();
+        startProgressbar();
+    } else { // hade navigation buttons for 1 slide only
+        $(id + ' .control_prev').hide();
+        $(id + ' .control_next').hide();
+    }
 
-        $(id + ' .progress').click(function () {
-            if (isPause === false) {
-                isPause = true;
-            } else {
-                isPause = false;
-            }
-        });
-    };
-});
+    // ARROW
+    $carousel.append('<div class="arrow arrow-prev"></div><div class="arrow arrow-next"></div>');
+
+    $('.arrow-next').on("click", function () {
+        /* $(this).data('clicked', true); */
+        percentTime = 0;
+        i++;
+
+        if (i <= indexImg) {
+            $img.css('display', 'none');
+            $currentImg = $img.eq(i);
+            $currentImg.fadeIn("slow");
+        } else {
+            i = 0;
+            $img.css('display', 'none');
+            $currentImg = $img.eq(i);
+            $currentImg.fadeIn("slow");
+        }
+
+        clicked = clicked + 1;
+
+        if (clicked <= 3) {
+            $('.carousel-buttons').removeClass("active");
+            $('#carousel' + clicked).addClass("active");
+        } else {
+            clicked = 1;
+            $('.carousel-buttons').removeClass("active");
+            $('#carousel' + clicked).addClass("active");
+        }
+
+    });
+
+    $('.arrow-prev').on("click", function () {
+        i--;
+        percentTime = 0;
+
+        if (i >= 0) {
+            $img.css('display', 'none');
+            $currentImg = $img.eq(i);
+            $currentImg.fadeIn("slow");
+        } else {
+            i = indexImg;
+            $img.css('display', 'none');
+            $currentImg = $img.eq(i);
+            $currentImg.fadeIn("slow");
+        }
+
+        clicked = clicked - 1;
+
+        if (clicked > 0) {
+            $('.carousel-buttons').removeClass("active");
+            $('#carousel' + clicked).addClass("active");
+        } else {
+            clicked = indexImg + 1;
+            $('.carousel-buttons').removeClass("active");
+            $('#carousel' + clicked).addClass("active");
+        }
+    });
+
+    // Defilement auto
+    function slideImg() {
+
+        if (i < indexImg) {
+            i++;
+        } else {
+            i = 0;
+        }
+
+        $img.css('display', 'none');
+        $currentImg = $img.eq(i);
+        $currentImg.fadeIn("slow");
+
+        clicked = i + 1;
+        $('.carousel-buttons').removeClass("active");
+        $('#carousel' + clicked).addClass("active");
+
+    }
+
+    // DOTS
+    $carousel.append('<div class="carousel-buttons-container"><ul class="dots">');
+    let addId = i + 1;
+
+    for (let a = 0; a <= indexImg; a++) {
+        $('.dots').append('<li class="carousel-buttons" id="carousel' + addId++ + '"></li>');
+    }
+
+    $('.carousel-buttons').click(function () {
+        percentTime = 0;
+
+        let findIdClicked = $(this).attr("id");
+        let splitString = findIdClicked.split("carousel");
+        let findTheNumb = splitString[1];
+
+        $img.css('display', 'none');
+        $currentImg = $img.eq(findTheNumb - 1);
+        $currentImg.fadeIn("slow");
+
+        $(".carousel-buttons").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $('.carousel-buttons-container').find("li").first().addClass("active");
+
+}
